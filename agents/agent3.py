3cd84b4ab1dc1c3e9e56fc5ae9e2f716e292dc85
yunlanli <chnsidney@gmail.com>
2022-10-25 17:47:24 -0400

    working huffman codec

--- agents/agent3.py
+++ agents/agent3.py
@@ -1,8 +1,32 @@
+from typing import List, Optional
+
+from dahuffman import load_shakespeare, HuffmanCodec
+from bitstring import Bits
+
+
+class Huffman:
+
+    def __init__(self, dictionary: Optional[List[str]] = None) -> None:
+        if dictionary is not None:
+            self.codec = HuffmanCodec.from_data(''.join(dictionary))
+        else:
+            self.codec = load_shakespeare()
+
+    def encode(self, msg: str) -> Bits:
+        print(f'[ Huffman.encode ] msg: {msg}')
+        bytes = self.codec.encode(msg)
+        bits = Bits(bytes=bytes)
+
+        return bits
+
+    def decode(self, bits: Bits) -> str:
+        return self.codec.decode(bits.tobytes())
+
+
 class Agent:
     def __init__(self):
         self.stop_card = 51
         self.trash_cards = list(range(32, 51))
-        
 
     def encode(self, message):
         encoded_message = []
@@ -25,3 +49,35 @@ class Agent:
     def get_encoded_message(self, deck):
         deck.index(self.stop_card)
         return deck[deck.index(self.stop_card)+1:]
+
+
+# -----------------------------------------------------------------------------
+#   Unit Tests
+# -----------------------------------------------------------------------------
+
+def test_huffman_codec():
+    # Note: the shakespeare codec doesn't seem to be able to handle punctuations
+    cases = ['group 3', 'magic code']
+
+    huffman = Huffman()
+    for tc in cases:
+        orig = tc
+        encoded = huffman.encode(orig)
+        decoded = huffman.decode(encoded)
+
+        assert type(encoded) == Bits, 'error: encoded message is not of type Bits!'
+        assert orig == decoded, 'error: decoded message is not the same as the original'
+
+    print('PASSED: Huffman codec using pre-traind shakespeare text')
+
+    cases = ['group 3', 'magic code', 'hi!']
+    huffman = Huffman(dictionary=cases)
+    for tc in cases:
+        orig = tc
+        encoded = huffman.encode(orig)
+        decoded = huffman.decode(encoded)
+
+        assert type(encoded) == Bits, 'error: encoded message is not of type Bits!'
+        assert orig == decoded, 'error: decoded message is not the same as the original'
+
+    print('PASSED: Huffman codec using dictionary')
