f47a939c64417b105d01c0f28053feffc50377c6
Mauricio Guerrero <65383117+Mg4145@users.noreply.github.com>
2022-10-25 20:12:50 -0400

    Working on agent3

--- agents/agent3.py
+++ agents/agent3.py
@@ -3,6 +3,7 @@ from typing import List, Optional
 
 from dahuffman import load_shakespeare, HuffmanCodec
 from bitstring import Bits
+import numpy as np
 
 
 log_level = logging.DEBUG
@@ -13,26 +14,35 @@ logger.setLevel(log_level)
 logger.addHandler(logging.FileHandler(log_file))
 
 
-def debug(*args):
+def debug(*args) -> None:
     logger.info(' '.join(args))
 
 
 class Huffman:
 
-    def __init__(self, dictionary: Optional[List[str]] = None) -> None:
+    def __init__(
+            self,
+            dictionary: Optional[List[str]] = None
+    ) -> None:
         if dictionary is not None:
             self.codec = HuffmanCodec.from_data(''.join(dictionary))
         else:
             self.codec = load_shakespeare()
 
-    def encode(self, msg: str) -> Bits:
+    def encode(
+            self,
+            msg: str
+    ) -> Bits:
         bytes = self.codec.encode(msg)
         bits = Bits(bytes=bytes)
 
         debug('[ Huffman.encode ]', f'msg: {msg} -> bits: {bits.bin}')
         return bits
 
-    def decode(self, bits: Bits) -> str:
+    def decode(
+            self,
+            bits: Bits
+    ) -> str:
         decoded = self.codec.decode(bits.tobytes())
 
         debug('[ Huffman.decode ]', f'bits: {bits.bin} -> msg: {decoded}')
@@ -40,30 +50,50 @@ class Huffman:
 
 
 class Agent:
-    def __init__(self):
+    def __init__(
+            self
+    ) -> None:
         self.stop_card = 51
         self.trash_cards = list(range(32, 51))
+        self.rng = np.random.default_rng(seed=42)
+        self.huff = Huffman()
 
-    def encode(self, message):
+    def encode(
+            self,
+            message
+    ) -> List[int]:
         encoded_message = []
 
-        useless_cards = [card for card in range(
-            0, 32) if card not in encoded_message]
-        deck = self.trash_cards + useless_cards + \
-            [self.stop_card] + encoded_message
+        print(f'this is the message:\t{message}')
+        debug('[ Agent.encode ]', f'this is the message:\t{message}')
+        print(self.huff.encode(message))
+
+        useless_cards = [card for card in range(0, 32)
+                         if card not in encoded_message]
+        deck = self.trash_cards + useless_cards + [self.stop_card] + encoded_message
+
         return deck
 
-    def decode(self, deck):
+    def decode(
+            self,
+            deck
+    ) -> str:
         deck = self.remove_trash_cards(deck)
         deck = self.get_encoded_message(deck)
         return "NULL"
 
-    def remove_trash_cards(self, deck):
+    def remove_trash_cards(
+            self,
+            deck
+    ) -> List[int]:
         for i in self.trash_cards:
             deck.remove(i)
         return deck
 
-    def get_encoded_message(self, deck):
+    def get_encoded_message(
+            self,
+            deck
+    ) -> List[int]:
         deck.index(self.stop_card)
         return deck[deck.index(self.stop_card)+1:]
 
@@ -95,8 +125,7 @@ def test_huffman_codec():
         encoded = huffman.encode(orig)
         decoded = huffman.decode(encoded)
 
-        assert type(
-            encoded) == Bits, 'error: encoded message is not of type Bits!'
+        assert type(encoded) == Bits, 'error: encoded message is not of type Bits!'
         assert orig == decoded, 'error: decoded message is not the same as the original'
 
     print('PASSED: Huffman codec using dictionary')
