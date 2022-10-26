286a4fd9c67855a97d30ec2c2235ece95a4134ff
Shreeyak <shreeyak.sajjan@gmail.com>
2022-10-26 12:09:07 -0400

    Added encode/decode funcs and tests

--- agents/agent1.py
+++ agents/agent1.py
@@ -1,4 +1,10 @@
+import random
 from itertools import permutations
+
+import numpy as np
+
+import cards
+
 
 class Agent:
     def __init__(self):
@@ -13,18 +19,42 @@ class Agent:
         self.valid_encodings = self.all_encodings[:num_valid]
 
         self.dict_encode = {str(idx): self.valid_encodings[idx] for idx in range(len(self.valid_encodings))}
-        self.dict_decode = {self.valid_encodings[idx]: idx for idx in range(len(self.valid_encodings))}
+        self.dict_decode = {self.valid_encodings[idx]: str(idx) for idx in range(len(self.valid_encodings))}
+        self.seed = 0
+        self.rng = np.random.default_rng(self.seed)
 
     def encode(self, message):
         if message not in self.dict_encode:
             return ValueError(f"message is not valid. Must be an int less than: {len(self.valid_encodings)}")
 
-        return list(range(52))
+        seq_encode = list(self.dict_encode[message])
+        # Scramble the rest of the cards above message
+        seq_rand = list(range(0, 52-self.encode_len))
+        self.rng.shuffle(seq_rand)
+        seq_total = seq_rand + seq_encode
+        return seq_total
 
     def decode(self, deck):
-        return "NULL"
+        encoding = [c for c in deck if c in self.valid_cards]
+        encoding = tuple(encoding)
+        if encoding in self.valid_encodings:
+            return self.dict_decode[encoding]
+        else:
+            return "NULL"
 
 
 if __name__ == "__main__":
     agent = Agent()
+    deck = agent.encode("1")
+    valid_deck = cards.valid_deck(deck)
+    msg = agent.decode(deck)
+
+    # Check NULL redundancy
+    count_null = 0
+    for idx in range(1000):
+        rng = np.random.default_rng(idx)
+        deck_r = cards.generate_deck(rng, random=True)
+        msg_r = agent.decode(deck_r)
+        if msg_r == "NULL":
+            count_null += 1
     print("Done")
