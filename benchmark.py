6a0448e0ae634ac7b40dfb89d07cbdcf771a9f35
Jeff Tao <jatao88@gmail.com>
2022-10-27 12:18:55 -0400

    Add argparse to benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,20 +1,54 @@
+import argparse
 import multiprocessing
 import random
+from importlib import import_module
 from itertools import product
 from string import ascii_lowercase
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-from agents.agent8 import Agent
+# ==============
+# Argparse setup
+# ==============
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--agent",
+    "-a",
+    type=int,
+    required=True,
+    help="which agent to use from 1 to 8",
+)
+parser.add_argument(
+    "--trials",
+    "-t",
+    default=50,
+    type=int,
+    help="number of trials per length/shuffle datapoint. defaults to 50",
+)
+parser.add_argument(
+    "--disable-threading",
+    "-d",
+    default=False,
+    action="store_true",
+    help="disable multithreading for benchmarking. makes script run on systems where multithreading isn't working but makes benchmark run more slowly",
+)
+pargs = parser.parse_args()
 
-TRIALS = 50
+agent_name = f"agent{pargs.agent}"
+agent_module = import_module(f".{agent_name}", "agents")
 
 
+# ==================
+# Message Generators
+# ==================
 def get_uniform_message(length: int) -> str:
     return "".join(random.choices(ascii_lowercase, k=length))
 
 
+# ===========================
+# Benchmarking infrastructure
+# ===========================
 def shuffle(n, deck):
     rng = np.random.default_rng()
     shuffles = rng.integers(0, 52, n)
@@ -26,10 +60,10 @@ def shuffle(n, deck):
 
 
 def run_trial(args: tuple[int, int]) -> tuple[int, int, float]:
-    agent = Agent()
+    agent = agent_module.Agent()
     length, n = args
     successes = 0
-    for _ in range(TRIALS):
+    for _ in range(pargs.trials):
         word = get_uniform_message(length)
         deck = agent.encode(word)
         shuffled = shuffle(n, deck)
@@ -37,14 +71,17 @@ def run_trial(args: tuple[int, int]) -> tuple[int, int, float]:
 
         if word == out:
             successes += 1
-    return length, n, successes / TRIALS
+    return length, n, successes / pargs.trials
 
 
 lengths = list(range(1, 11))
 shuffle_amounts = list(range(100))
 work = list(product(lengths, shuffle_amounts))
 pool = multiprocessing.Pool(multiprocessing.cpu_count())
-results = pool.imap_unordered(run_trial, work, chunksize=4)
+if pargs.disable_threading:
+    results = map(run_trial, work)
+else:
+    results = pool.imap_unordered(run_trial, work, chunksize=4)
 
 print("Collecting results...")
 collected_results = {}
@@ -54,6 +91,9 @@ for result in results:
         collected_results[length] = []
     collected_results[length].append((n, recovery_rate))
 
+# ======================
+# Plot benchmark results
+# ======================
 print("Plotting...")
 for length in collected_results:
     ys = [recovery_rate for _, recovery_rate in sorted(collected_results[length])]
