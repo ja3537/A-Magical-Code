7de13dbcbbda600ffd84b3be44fbd8ae8bda6dc2
Noah Silverstein <i.noah@columbia.edu>
2022-10-31 13:33:43 -0400

    Switch to enum and support multiple encoding freqs

--- agents/agent4.py
+++ agents/agent4.py
@@ -3,69 +3,33 @@ import numpy as np
 from typing import List
 import math
 from pearhash import PearsonHasher
-import binascii
+from enum import Enum
 from dahuffman import HuffmanCodec
 
+class Domain(Enum):
+    ALL = 0
+    NUM = 1
+    LAT_LONG = 2
+    DATE = 3
+
+DomainFrequencies = {
+    # reference of English letter frequencies: https://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+    Domain.ALL: {"a": 8.12, "b": 1.49, "c": 2.71, "d": 4.32, "e": 12.02, "f": 2.30, "g": 2.03, "h": 5.92, "i": 7.31, "j": 0.10, "k": 0.69, "l": 3.98, "m": 2.61, "n": 6.95, "o": 7.68, "p": 1.82, "q": 0.11, "r": 6.02, "s": 6.28, "t": 9.10, "u": 2.88, "v": 1.11, "w": 2.09, "x": 0.17, "y": 2.11, "z": 0.07, " ": 0.11, "\t": 0.10, ".": 6.97, ",": 5.93, "'": 1.53, "\"": 1.33, ":": 0.90, "-": 0.77, ";": 0.74, "?": 0.43, "!": 0.39, "0": 0.09, "1": 0.08, "2": 0.07, "3": 0.06, "4": 0.05, "5": 0.04, "6": 0.03, "7": 0.02, "8": 0.01, "9": 0.005},
+}
 
 class Agent:
     def __init__(self):
-        # reference of English letter frequencies: https://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
-        self.frequencies = {
-            "a": 8.12,
-            "b": 1.49,
-            "c": 2.71,
-            "d": 4.32,
-            "e": 12.02,
-            "f": 2.30,
-            "g": 2.03,
-            "h": 5.92,
-            "i": 7.31,
-            "j": 0.10,
-            "k": 0.69,
-            "l": 3.98,
-            "m": 2.61,
-            "n": 6.95,
-            "o": 7.68,
-            "p": 1.82,
-            "q": 0.11,
-            "r": 6.02,
-            "s": 6.28,
-            "t": 9.10,
-            "u": 2.88,
-            "v": 1.11,
-            "w": 2.09,
-            "x": 0.17,
-            "y": 2.11,
-            "z": 0.07,
-            ".": 6.97,
-            ",": 5.93,
-            "'": 1.53,
-            "\"": 1.33,
-            ":": 0.90,
-            "-": 0.77,
-            ";": 0.74,
-            "?": 0.43,
-            "!": 0.39,
-            "\t": 0.10,
-            " ": 0.11,
-            "0": 0.09,
-            "1": 0.08,
-            "2": 0.07,
-            "3": 0.06,
-            "4": 0.05,
-            "5": 0.04,
-            "6": 0.03,
-            "7": 0.02,
-            "8": 0.01,
-            "9": 0.005
-        }
         self.rng = np.random.default_rng(seed=42)
 
-    def string_to_binary(self, message, domain_type):
-        return ''.join(format(ord(i), 'b') for i in message)
+    def string_to_binary(self, message, domain):
+        bytes_repr = HuffmanCodec.from_frequencies(self.get_domain_frequencies(domain)).encode(message)
+        binary_repr = bin(int(bytes_repr.hex(), 16))[2:].zfill(8)
+        return binary_repr
 
-    def binary_to_string(self, binary, domain_type):
-        return ''.join(chr(int(binary[i * 7:i * 7 + 7], 2)) for i in range(len(binary) // 7))
+    def binary_to_string(self, binary, domain):
+        message_byte = int(binary, 2).to_bytes((int(binary, 2).bit_length() + 7) // 8, 'big')
+        message = HuffmanCodec.from_frequencies(self.get_domain_frequencies(domain)).decode(message_byte)
+        return message
 
     def deck_encoded(self, message_cards):
         # message_cards: cards for message
@@ -116,19 +80,22 @@ class Agent:
         hex_hash = hasher.hash(str(int(bit_string, 2)).encode()).hexdigest()
         return bin(int(hex_hash, 16))[2:].zfill(8)
 
-    def domain_to_binary(self, domain_type):
-        return bin(int(domain_type))[2:].zfill(2)
+    def domain_to_binary(self, domain_type: Domain):
+        return bin(int(domain_type.value))[2:].zfill(2)
 
     def get_domain_type(self, message):
         message_no_space = "".join(message.split())
         if message.isalnum() or message_no_space.isalnum():
-            return '0'
+            return Domain.NUM
         elif self.is_lat_long(message_no_space):  # ex: 21 18.41', 157 51.50'
-            return '1'
+            return Domain.LAT_LONG
         elif self.is_date(message_no_space):
-            return '2'
+            return Domain.DATE
         else:
-            return '3'  # do generic encoding
+            return Domain.ALL  # do generic encoding
+        
+    def get_domain_frequencies(self, domain):
+        return DomainFrequencies[domain] if domain in DomainFrequencies.keys() else DomainFrequencies[Domain.ALL]
 
     def is_lat_long(self, message):
         # only numbers, commas, apostrophes
@@ -144,16 +111,16 @@ class Agent:
             return 'NULL'
 
         message_no_space = "".join(message.split())
-        if domain_type == 0:
+        if domain_type == Domain.NUM:
             if not (message.isalnum() or message_no_space.isalnum()):
                 return 'NULL'
-        elif domain_type == 1:
+        elif domain_type == Domain.LAT_LONG:
             if not self.is_lat_long(message_no_space):
                 return 'NULL'
-        elif domain_type == 2:
+        elif domain_type == Domain.DATE:
             if not self.is_date(message_no_space):
                 return 'NULL'
-        elif domain_type == 3:
+        elif domain_type == Domain.ALL:
             if not all(ord(c) < 128 and ord(c) > 32 for c in message):
                 return 'NULL'
         return message
@@ -164,13 +131,7 @@ class Agent:
         domain_type = self.get_domain_type(message)
         domain_binary = self.domain_to_binary(domain_type)
 
-        bytes_repr = HuffmanCodec.from_frequencies(
-            self.frequencies).encode(message)
-        binary_repr = bin(int(bytes_repr.hex(), 16))[2:].zfill(8)
-
-        # integer_repr = int.from_bytes(bytes_repr, "big")
-
-        # binary_repr = self.string_to_binary(message, domain_type)
+        binary_repr = self.string_to_binary(message, domain_type)
         binary_repr = binary_repr + self.get_hash(binary_repr) + domain_binary
         integer_repr = int(binary_repr, 2)
 
@@ -198,11 +159,7 @@ class Agent:
             domain_type = int(domain_bits, 2)
 
             if len(hash_bits) == 8 and len(message_bits) and hash_bits == self.get_hash(message_bits) and len(domain_bits) == 2:
-                # message = self.binary_to_string(message_bits, domain_type)
-                message_byte = int(message_bits, 2).to_bytes(
-                    (int(message_bits, 2).bit_length() + 7) // 8, 'big')
-                message = HuffmanCodec.from_frequencies(
-                    self.frequencies).decode(message_byte)
+                message = self.binary_to_string(message_bits, domain_type)
 
                 # TODO: ugly hack to fix the checksum, can be improved
                 if meet_checksum_count > 2:
