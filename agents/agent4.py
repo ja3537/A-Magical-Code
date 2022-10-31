23b8d96a56bf1afc0e12cb9956e37befbb316374
Xzh0u <xzh0u.sxz@gmail.com>
2022-10-31 01:27:16 -0400

    WIP: huffman encoding.

--- agents/agent4.py
+++ agents/agent4.py
@@ -4,10 +4,39 @@ from typing import List
 import math
 from pearhash import PearsonHasher
 import binascii
+from dahuffman import HuffmanCodec
 
 
 class Agent:
     def __init__(self):
+        self.frequencies = {
+            "a": 0.08167,
+            "b": 0.01492,
+            "c": 0.02782,
+            "d": 0.04253,
+            "e": 0.12702,
+            "f": 0.02228,
+            "g": 0.02015,
+            "h": 0.06094,
+            "i": 0.06966,
+            "j": 0.00153,
+            "k": 0.00772,
+            "l": 0.04025,
+            "m": 0.02406,
+            "n": 0.06749,
+            "o": 0.07507,
+            "p": 0.01929,
+            "q": 0.00095,
+            "r": 0.05987,
+            "s": 0.06327,
+            "t": 0.09056,
+            "u": 0.02758,
+            "v": 0.00978,
+            "w": 0.02360,
+            "x": 0.00150,
+            "y": 0.01974,
+            "z": 0.00074,
+        }
         self.rng = np.random.default_rng(seed=42)
 
     def string_to_binary(self, message, domain_type):
@@ -72,17 +101,18 @@ class Agent:
         message_no_space = "".join(message.split())
         if message.isalnum() or message_no_space.isalnum():
             return '0'
-        elif self.is_lat_long(message_no_space): # ex: 21 18.41', 157 51.50'
+        elif self.is_lat_long(message_no_space):  # ex: 21 18.41', 157 51.50'
             return '1'
         elif self.is_date(message_no_space):
             return '2'
         else:
-            return '3' # do generic encoding
+            return '3'  # do generic encoding
 
     def is_lat_long(self, message):
         # only numbers, commas, apostrophes
         return all([ch.isdigit() or ch == ',' or ch == "'" for ch in message]) and \
-               any(ch.isdigit() for ch in message) and (',' in message) and ("'" in message)
+            any(ch.isdigit() for ch in message) and (
+                ',' in message) and ("'" in message)
 
     def is_date(self, message):
         return all([ch.isalnum() or ch == ',' for ch in message])
@@ -111,8 +141,13 @@ class Agent:
 
         domain_type = self.get_domain_type(message)
         domain_binary = self.domain_to_binary(domain_type)
+        
+        bytes_repr = HuffmanCodec.from_frequencies(
+            self.frequencies).encode(message)
+        binary_repr = bin(int(bytes_repr.hex(), 16))[2:].zfill(8)
+        integer_repr = int.from_bytes(bytes_repr, "big")
 
-        binary_repr = self.string_to_binary(message, domain_type)
+        # binary_repr = self.string_to_binary(message, domain_type)
         binary_repr = binary_repr + self.get_hash(binary_repr) + domain_binary
         #print(message, domain_type)
         integer_repr = int(binary_repr, 2)
@@ -123,7 +158,8 @@ class Agent:
                 num_cards_to_encode = n
                 break
         message_start_idx = len(deck) - num_cards_to_encode
-        message_cards = self.num_to_cards(integer_repr, deck[message_start_idx:])
+        message_cards = self.num_to_cards(
+            integer_repr, deck[message_start_idx:])
         return self.deck_encoded(message_cards)
 
     def decode(self, deck):
@@ -139,10 +175,15 @@ class Agent:
             domain_type = int(domain_bits, 2)
 
             if len(hash_bits) == 8 and len(message_bits) and hash_bits == self.get_hash(message_bits) and len(domain_bits) == 2:
-                message = self.binary_to_string(message_bits, domain_type)
+                # message = self.binary_to_string(message_bits, domain_type)
+                message_byte = int(message_bits, 2).to_bytes(
+                    (int(message_bits, 2).bit_length() + 7) // 8, 'big')
+                message = HuffmanCodec.from_frequencies(
+                    self.frequencies).decode(message_byte)
                 break
 
         return self.check_decoded_message(message, domain_type)
+
 
 if __name__ == "__main__":
     agent = Agent()
