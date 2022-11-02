a638a5ce234a083bf2ddd48c150b9f74a60b4d3e
Noah Silverstein <i.noah@columbia.edu>
2022-11-01 22:10:19 -0400

    Cleanup

--- agents/agent4.py
+++ agents/agent4.py
@@ -1,3 +1,4 @@
+from operator import length_hint
 from cards import generate_deck
 import numpy as np
 from typing import List, Dict
@@ -5,10 +6,11 @@ import math
 from pearhash import PearsonHasher
 from enum import Enum
 from dahuffman import HuffmanCodec
+from collections import namedtuple
 
 class Domain(Enum):
     ALL = 0
-    NUM = 1
+    ALPHA_NUM = 1
     LAT_LONG = 2
     DATE = 3
 
@@ -16,6 +18,8 @@ DomainFrequencies = {
     # reference of English letter frequencies: https://pi.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
     Domain.ALL: {"a": 8.12, "b": 1.49, "c": 2.71, "d": 4.32, "e": 12.02, "f": 2.30, "g": 2.03, "h": 5.92, "i": 7.31, "j": 0.10, "k": 0.69, "l": 3.98, "m": 2.61, "n": 6.95, "o": 7.68, "p": 1.82, "q": 0.11, "r": 6.02, "s": 6.28, "t": 9.10, "u": 2.88, "v": 1.11, "w": 2.09, "x": 0.17, "y": 2.11, "z": 0.07, " ": 0.11, "\t": 0.10, ".": 6.97, ",": 5.93, "'": 1.53, "\"": 1.33, ":": 0.90, "-": 0.77, ";": 0.74, "?": 0.43, "!": 0.39, "0": 0.09, "1": 0.08, "2": 0.07, "3": 0.06, "4": 0.05, "5": 0.04, "6": 0.03, "7": 0.02, "8": 0.01, "9": 0.005},
 }
+
+EncodedBinary = namedtuple('EncodedBinary', ['message_bits', 'domain_bits', 'checksum_bits'])
 
 class Agent:
     def __init__(self):
@@ -81,12 +85,12 @@ class Agent:
         return bin(int(hex_hash, 16))[2:].zfill(8)
 
     def domain_to_binary(self, domain_type: Domain) -> str:
-        return bin(int(domain_type.value))[2:].zfill(2)
+        return bin(int(domain_type.value))[3:].zfill(3)
 
     def get_domain_type(self, message: str) -> Domain:
         message_no_space = "".join(message.split())
         if message.isalnum() or message_no_space.isalnum():
-            return Domain.NUM
+            return Domain.ALPHA_NUM
         elif self.is_lat_long(message_no_space):  # ex: 21 18.41', 157 51.50'
             return Domain.LAT_LONG
         elif self.is_date(message_no_space):
@@ -106,12 +110,12 @@ class Agent:
     def is_date(self, message: str) -> bool:
         return all([ch.isalnum() or ch == ',' for ch in message])
 
-    def check_decoded_message(self, message: str, domain_type: Domain) -> str:
+    def check_decoded_message(self, message: str, domain_type) -> str:
         if message == '':
             return 'NULL'
 
         message_no_space = "".join(message.split())
-        if domain_type == Domain.NUM:
+        if domain_type == Domain.ALPHA_NUM:
             if not (message.isalnum() or message_no_space.isalnum()):
                 return 'NULL'
         elif domain_type == Domain.LAT_LONG:
@@ -125,14 +129,19 @@ class Agent:
                 return 'NULL'
         return message
 
+    def get_binary_parts(self, binary: str) -> EncodedBinary:
+        checksum_bits = binary[-8:]
+        domain_bits = binary[-11:-8]
+        message_bits = binary[:-11]
+        return EncodedBinary(message_bits, domain_bits, checksum_bits)
+
     def encode(self, message: str) -> List[int]:
         deck = generate_deck(self.rng)
 
         domain_type = self.get_domain_type(message)
-        domain_binary = self.domain_to_binary(domain_type)
 
         binary_repr = self.string_to_binary(message, domain_type)
-        binary_repr = binary_repr + self.get_hash(binary_repr) + domain_binary
+        binary_repr = binary_repr + self.domain_to_binary(domain_type) + self.get_hash(binary_repr)
         integer_repr = int(binary_repr, 2)
 
         num_cards_to_encode = 1
@@ -147,25 +156,26 @@ class Agent:
 
     def decode(self, deck: List[int]) -> str:
         message = ''
+        domain_type = None
         meet_checksum_count = 0
         for n in reversed(range(1, 51)):
             encoded_cards = self.get_encoded_cards(deck, n)
             integer_repr = self.cards_to_num(encoded_cards)
             binary_repr = bin(int(integer_repr))[2:]
-            message_bits = binary_repr[:-10]
-            middle_man = binary_repr[:-2]
-            hash_bits = middle_man[-8:]
-            domain_bits = binary_repr[-2:]
-            domain_type = int(domain_bits, 2)
+            parts = self.get_binary_parts(binary_repr)
+            len_metadata_bits = len(parts.domain_bits) + len(parts.checksum_bits)
 
-            if len(hash_bits) == 8 and len(message_bits) and hash_bits == self.get_hash(message_bits) and len(domain_bits) == 2:
-                message = self.binary_to_string(message_bits, domain_type)
+            if len_metadata_bits == 11 and parts.message_bits and parts.checksum_bits == self.get_hash(parts.message_bits):
+                domain_type = int(parts.domain_bits, 2)
+                message = self.binary_to_string(parts.message_bits, domain_type)
 
                 # TODO: ugly hack to fix the checksum, can be improved
                 if meet_checksum_count > 2:
                     break
                 meet_checksum_count += 1
                 # print(flag, ":" + message)
+        if meet_checksum_count < 2:
+            return 'NULL'
 
         return self.check_decoded_message(message, domain_type)
 
