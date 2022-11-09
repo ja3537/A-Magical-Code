d121fc6aefb546c5adcc07ec012c671ee0674d2b
Maximo Oen <mmo2134@columbia.edu>
2022-11-08 20:38:16 -0500

    Added infrastructure for getting encoding from other generators

--- messages/agent6/generate_corpus_probabilities/analyze_generator.py
+++ messages/agent6/generate_corpus_probabilities/analyze_generator.py
@@ -2,16 +2,21 @@ import os
 
 if __name__ == "__main__":
 
+	agent_list = {}
+
+	os.chdir(os.path.dirname(os.path.realpath(__file__)))
 	agents = [1,2,3,4,5,6,7,8]
 
 	for agent_number in agents:
 
 		count = {}
-		agent_name = "agent{}".format(agent_number)
+		agent_name = "./agent{}".format(agent_number)
+		print(agent_name)
 
 		for filename in os.listdir(agent_name):
+			print(agent_name + "/" + filename)
 
-			f = open(filename, "r")
+			f = open(agent_name + "/" + filename, "r")
 			
 			for line in f:
 
@@ -23,3 +28,7 @@ if __name__ == "__main__":
 					count[c] = count[c]+1 if c in count else 1
 		
 			f.close()
+		
+		agent_list[agent_number] = (count)
+
+	print(agent_list)
